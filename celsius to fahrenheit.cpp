89c45a5391458d4b970b60576227e696f8865ba3
Kusuma853 <kusumalekkalapudi98@gmail.com>
2025-10-28 20:38:10 +0530

    Add files via upload

---
+++ celsius to fahrenheit.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float fahrenheit,celsius;
+	printf("\ntemperature in celsius : ");
+	scanf("%f",&celsius);
+	fahrenheit=(celsius*1.8)+32;
+	printf("\ntemperature in fahrenheit : %f",fahrenheit);
+	return 0;
+}
