a7c8fd0976559d40f03587cb571f2822ffb8f652
L Kusuma <kusumalekkalapudi98@gmail.com>
2025-11-16 07:09:13 +0530

    Add files via upload

---
+++ string 3.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	char str3[100];
+	printf("\nenter any string into str3:");
+	gets(str3);
+	puts(str3);
+	return 0;
+}
---
+++ string 4.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	char str4[100];
+	printf("\nenter any string into str4:");
+	fgets(str4,100,stdin);
+	puts(str4);
+	return 0;
+}
